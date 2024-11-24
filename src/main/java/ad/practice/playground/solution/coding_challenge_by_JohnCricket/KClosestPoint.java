680817266a1ff5191281036117f34445ac6a3c84
ADS77 <das.cse.kuet@gmail.com>
2024-11-25 01:38:28 +0600

    implementation of EgyptianFraction

--- src/main/java/ad/practice/playground/solution/coding_challenge_by_JohnCricket/KClosestPoint.java
+++ src/main/java/ad/practice/playground/solution/coding_challenge_by_JohnCricket/KClosestPoint.java
@@ -1,4 +1,4 @@
-package ad.practice.playground.solution;
+package ad.practice.playground.solution.coding_challenge_by_JohnCricket;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -60,8 +60,9 @@ public class KClosestPoint {
     }
 }
 
-//problem statement :
+
 /*
+problem statement :
 Given a list of points, a central point, and an integer k, find the nearest k points from the central point.
 For example, given the list of points [(0, 0), (5, 4), (3, 1)], the central point (1, 2), and k = 2, return [(0, 0), (3, 1)].
 */
--- src/main/java/ad/practice/playground/solution/coding_challenge_by_JohnCricket/RunLengthEncoding.java
+++ src/main/java/ad/practice/playground/solution/coding_challenge_by_JohnCricket/RunLengthEncoding.java
@@ -1,4 +1,4 @@
-package ad.practice.playground.solution;
+package ad.practice.playground.solution.coding_challenge_by_JohnCricket;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -70,3 +70,9 @@ public class RunLengthEncoding {
         return res;
     }
 }
+
+/*
+Problem statement :
+Run-length encoding is a fast and simple method of encoding strings. The basic idea is to represent repeated successive characters as a single count and character. For example, the string "AAAABBBCCDAA" would be encoded as "4A3B2C1D2A".
+Implement run-length encoding and decoding. You can assume the string to be encoded have no digits and consists solely of alphabetic characters. You can assume the string to be decoded is valid.
+*/
